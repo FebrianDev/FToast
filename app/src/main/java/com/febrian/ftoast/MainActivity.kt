8e77d9d10e9726d68864be115404021e49726afd
Febrian <febrian26022001@gmail.com>
2022-05-30 08:58:18 +0700

    Test library

--- app/src/main/java/com/febrian/ftoast/MainActivity.kt
+++ app/src/main/java/com/febrian/ftoast/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val toast = FToast(applicationContext)
-        toast.show("Hello World")
+        toast.show("Hello Toast!")
 
     }
 }
